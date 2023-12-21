360e3f8d09eb4b3ed07762f0f7706c9f8576a67f
Wojciech Nawrocki <wjnawrocki@protonmail.com>
2023-12-22 00:59:42 +0100

    feat: duper duper duper

--- lakefile.lean
+++ lakefile.lean
@@ -1,7 +1,14 @@
 import Lake
 open Lake DSL
 
-package «lean-sat»
+package «lean-sat» where
+  moreLinkArgs := #["-L./.lake/packages/LeanCopilot/.lake/build/lib", "-lctranslate2"]
+
+-- Note: `std` is not required so that Lake selects a version matching mathlib.
+require mathlib from git "https://github.com/leanprover-community/mathlib4.git" @ "master"
+
+require LeanCopilot from git "https://github.com/lean-dojo/LeanCopilot" @ "v1.0.0"
+require Duper from git "https://github.com/leanprover-community/duper.git" @ "v0.0.5"
 
 @[default_target]
 lean_lib LeanSAT
@@ -11,6 +18,3 @@ lean_lib Examples
 lean_exe run_examples {
   root := `Examples
 }
-
--- Note: `std` is not required so that Lake selects a version matching mathlib.
-require mathlib from git "https://github.com/leanprover-community/mathlib4.git" @ "master"
