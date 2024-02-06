6aeb08a316558b25492cec996cfd5a533a889e5f
Wojciech Nawrocki <wjnawrocki@protonmail.com>
2024-02-05 20:56:53 -0500

    idk what the symbols mean

--- LeanSAT/Model/PropFun.lean
+++ LeanSAT/Model/PropFun.lean
@@ -144,6 +144,11 @@ scoped instance : SemanticEntails (PropAssignment ν) (PropFun ν) where
 
 open SemanticEntails renaming entails → sEntails
 
+instance (τ : PropAssignment ν) (φ : PropFun ν) : Decidable (τ ⊨ φ) :=
+  match h : φ.eval τ with
+    | true => isTrue h
+    | false => isFalse fun h' => nomatch h.symm.trans h'
+
 @[ext]
 theorem ext : (∀ (τ : PropAssignment ν), τ ⊨ φ₁ ↔ τ ⊨ φ₂) → φ₁ = φ₂ := by
   have ⟨φ₁, h₁⟩ := Quotient.exists_rep φ₁
