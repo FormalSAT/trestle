43aa1f9cc14bfc143258ecd6010c52b791b2a813
James Gallicchio <jamesgallicchio@gmail.com>
2024-02-13 20:48:45 -0500

    fix: LeanSAT.lean

--- LeanSAT.lean
+++ LeanSAT.lean
@@ -1,3 +1,35 @@
+/-
+Copyright (c) 2024 The LeanSAT Contributors.
+Released under the Apache License v2.0; see LICENSE for full text.
+
+Authors: James Gallicchio
+-/
+
+import LeanSAT.Data.Cnf
+import LeanSAT.Data.HashAssn
+import LeanSAT.Data.ICnf
+import LeanSAT.Data.Literal
+import LeanSAT.Encode.Card
+import LeanSAT.Encode.EncCNF
+import LeanSAT.Encode.Tseitin
+import LeanSAT.Encode.VEncCNF
+import LeanSAT.Model.OfFun
+import LeanSAT.Model.PropAssn
+import LeanSAT.Model.PropForm
+import LeanSAT.Model.PropVars
+import LeanSAT.Model.Quantifiers
+import LeanSAT.Model.Subst
+import LeanSAT.Solver.Impl.ApproxMCCommand
+import LeanSAT.Solver.Impl.CMSGenCommand
+import LeanSAT.Solver.Impl.D4Command
+import LeanSAT.Solver.Impl.DimacsCommand
+import LeanSAT.Solver.Impl.UniGenCommand
+import LeanSAT.Solver.Basic
+import LeanSAT.Solver.Dimacs
+import LeanSAT.Upstream.FinEnum
+import LeanSAT.Upstream.ToMathlib
+import LeanSAT.Upstream.ToStd
+
 /-! # LeanSAT
 
 TODO(JG): Insert top-level documentation here
