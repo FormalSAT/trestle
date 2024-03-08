9671632bce2fbbe6d36ef54cd944e69e81b5f054
James Gallicchio <jamesgallicchio@gmail.com>
2024-03-08 03:51:27 -0500

    start macro for cakelpr based axioms

--- Trestle/Solver/Impl/CakeLpr.lean
+++ Trestle/Solver/Impl/CakeLpr.lean
@@ -56,3 +56,40 @@ def CakeLpr (solverCmd : String) (solverFlags : Array String := #[]) (cakelprCmd
       return res
     else
       return .error
+
+namespace CakeLpr
+
+/-- Check that a CNF is unsat, using the CakeLPR verified pipeline.
+If the CNF is found to be unsat, inserts an axiom asserting this result.
+
+**Form 1:** Run a SAT solver and pipe the proof to CakeLPR.
+Note that this expects there to be a `Solver IO` instance in context.
+
+Example:
+`#cakelpr_check_unsat myCnf`
+results in the axiom
+`axiom myCnfIsUnsat : ∀ τ, τ ⊭ myCnf.toPropFun`
+
+**Form 2:** Run CakeLPR with a proof from a file.
+
+Example:
+`#cakelpr_check_unsat myCnf withProofFile "path/to/proof.lrat"`
+
+**Form 3:** Run CakeLPR with a compositional proof from a file and hash file.
+This is for cube & conquer proofs,
+where we have a "compositional file" (the top-level proof)
+and a "hash file" (storing hashes that evidence each cube's unsatisfiability)
+
+Example:
+`#cakelpr_check_unsat myCnf withCompFile "path/to/comp.lrat" withHashFile "path/to/hashes"`
+-/
+syntax "#cakelpr_check_unsat " ident : command
+
+@[inherit_doc LeanSAT.Solver.Impl.CakeLpr.«command#cakelpr_check_unsat_»]
+syntax "#cakelpr_check_unsat " ident " withProofFile " term : command
+
+@[inherit_doc LeanSAT.Solver.Impl.CakeLpr.«command#cakelpr_check_unsat_»]
+syntax "#cakelpr_check_unsat " ident " withCompFile " term " withHashFile " term : command
+
+macro_rules
+| `(#cakelpr_check_unsat $x) => do `(sorry)
