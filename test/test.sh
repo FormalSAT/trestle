303c2d8550727ac00a4bbe15ee86a86b192699e9
James Gallicchio <jamesgallicchio@gmail.com>
2024-03-14 00:28:03 -0400

    smaller tests

--- test/test.sh
+++ test/test.sh
@@ -1,14 +1,14 @@
-set -v
+set -o xtrace
 
 CNF=test.cnf
 CUBES=test.cubes
 COMP=test.comp
 
-TMPCNF=test_tmp.cnf
-TMPICNF=test_tmp.icnf
-TMPDRAT=test_tmp.drat
-TMPLRAT=test_tmp.lrat
-ITER=1
+TMPCNF=tmp.cnf
+TMPICNF=tmp.icnf
+TMPDRAT=tmp.drat
+TMPLRAT=tmp.lrat
+ITER=0
 
 VARS=$( head -n 1 $CNF | awk '{ print $3 }' )
 CLAUSES=$( head -n 1 $CNF | awk '{ print $4 }' )
@@ -21,18 +21,12 @@ echo $ASSUME_COUNT
 
 echo "p cnf $VARS $(($CLAUSES + $ASSUME_COUNT))" > $TMPCNF
 tail $CNF --lines=+2 >> $TMPCNF
-echo "$ASSUMES" | sed -r 's/([0-9]+) /\1 0\n/g' >> $TMPCNF
-
-#echo "p inccnf" > $TMPICNF
-#tail $CNF --lines=+2 >> $TMPICNF
-#(tail $CUBES --lines=+$(( $ITER+1 )) | head --lines=1) >> $TMPICNF
+echo -n "$ASSUMES" | sed -r 's/([0-9]+) /\1 0\n/g' >> $TMPCNF
 
 cadical $TMPCNF $TMPDRAT --quiet --no-binary
 
-#echo "p cnf $VARS $(($CLAUSES + 1))" > $TMPCNF
-#tail $CNF --lines=+2 >> $TMPCNF
-#(tail $COMP --lines=+$(( $ITER+1 )) | head --lines=1) >> $TMPCNF
-
 drat-trim $TMPCNF $TMPDRAT -L $TMPLRAT
+
+cake_lpr $TMPCNF $TMPLRAT
 
 cake_lpr $CNF $COMP $ITER-$(( $ITER+1 )) $TMPLRAT
