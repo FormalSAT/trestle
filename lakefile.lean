403dab1d0031f3ba5e002956c04dc53f1854e527
James Gallicchio <jamesgallicchio@gmail.com>
2024-04-16 16:23:48 -0400

    lakefile

--- lakefile.lean
+++ lakefile.lean
@@ -21,4 +21,4 @@ lean_exe «FormulaParser» {
 }
 
 -- Note: `mathlib` and `std` are obtained transitively so that the versions all match up
-require leancolls from git "https://github.com/digama0/LeanColls.git" @ "v4.7.0"
+require leancolls from git "https://github.com/JamesGallicchio/LeanColls.git" @ "v4.7.0"
