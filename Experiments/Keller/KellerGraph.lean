a6f620009bcc72c9c088655fc1bb9dee061b747c
James <jamesgallicchio@gmail.com>
2025-01-26 02:13:16 -0500

    feat: keller theory

--- Experiments/Keller/KellerGraph.lean
+++ Experiments/Keller/KellerGraph.lean
@@ -3,27 +3,29 @@ import Experiments.Keller.Upstream
 import Mathlib.Combinatorics.SimpleGraph.Basic
 import Mathlib.Combinatorics.SimpleGraph.Clique
 
+namespace Keller
+
 @[ext]
-structure KellerVertex (n s : Nat) where
+structure KVertex (n s : Nat) where
   bv : BitVec n
   colors : Vector (Fin s) n
 deriving Repr, DecidableEq
 
-namespace KellerVertex
+namespace KVertex
 
-nonrec def toString (v : KellerVertex n s) : String :=
+nonrec def toString (v : KVertex n s) : String :=
   s!"{v.bv};{v.colors.toList}"
 
-instance : ToString (KellerVertex n s) where
-  toString := KellerVertex.toString
+instance : ToString (KVertex n s) where
+  toString := KVertex.toString
 
-def flip (j : Fin n) (v : KellerVertex n s) : KellerVertex n s :=
+def flip (j : Fin n) (v : KVertex n s) : KVertex n s :=
   { bv := v.bv ^^^ BitVec.shiftLeft 1 j, colors := v.colors }
 
-@[simp] theorem colors_flip {j} {v : KellerVertex n s} : (flip j v).colors = v.colors := rfl
-@[simp] theorem bv_flip_eq {j} {v : KellerVertex (n+1) s} : (flip j v).bv[(↑j : Nat)] = !v.bv[j] := by
+@[simp] theorem colors_flip {j} {v : KVertex n s} : (flip j v).colors = v.colors := rfl
+@[simp] theorem bv_flip_eq {j} {v : KVertex (n+1) s} : (flip j v).bv[(↑j : Nat)] = !v.bv[j] := by
   simp [flip]
-@[simp] theorem bv_flip_ne {j j'} {v : KellerVertex (n+1) s} (h : j ≠ j') :
+@[simp] theorem bv_flip_ne {j j'} {v : KVertex (n+1) s} (h : j ≠ j') :
     (flip j v).bv[↑(j' : Nat)] = v.bv[j'] := by
   simp [flip]
   by_cases j' < j <;> simp [*]
@@ -31,67 +33,108 @@ def flip (j : Fin n) (v : KellerVertex n s) : KellerVertex n s :=
   · omega
   · simp [*]
 
-@[simp] def flip_flip (j : Fin n) {v : KellerVertex n s} : (v.flip j).flip j = v := by
+@[simp] theorem flip_flip (j : Fin n) {v : KVertex n s} : (v.flip j).flip j = v := by
   simp [flip, BitVec.xor_assoc]
 
-def permute (j : Fin n) (f : Fin s → Fin s) (v : KellerVertex n s) : KellerVertex n s :=
+theorem flip_comm (j₁ j₂ : Fin n) {v : KVertex n s} : (v.flip j₂).flip j₁ = (v.flip j₁).flip j₂ := by
+  simp [flip]; rw [BitVec.xor_assoc]
+  conv => enter [1,2]; rw [BitVec.xor_comm]
+  rw [BitVec.xor_assoc]
+
+def permute (j : Fin n) (f : Fin s → Fin s) (v : KVertex n s) : KVertex n s :=
   { bv := v.bv, colors := Vector.ofFn (fun j' => if j = j' then f v.colors[j'] else v.colors[j']) }
 
-@[simp] theorem bv_permute {j f} {v : KellerVertex n s} : bv (permute j f v) = v.bv := rfl
-@[simp] theorem colors_permute_eq {j f} {v : KellerVertex n s} {hj} :
+@[simp] theorem bv_permute {j f} {v : KVertex n s} : bv (permute j f v) = v.bv := rfl
+@[simp] theorem colors_permute_eq {j f} {v : KVertex n s} {hj} :
     (permute j f v).colors[(j : Nat)]'hj = f v.colors[j] := by
   simp [permute, Vector.ofFn, getElem]; simp [Vector.get]
-@[simp] theorem colors_permute_ne {j j' f} {v : KellerVertex n s} {hj'} (h : j ≠ j') :
+@[simp] theorem colors_permute_ne {j j' f} {v : KVertex n s} {hj'} (h : j ≠ j') :
     (permute j f v).colors[(j' : Nat)]'hj' = v.colors[j'] := by
   simp [permute, Vector.ofFn, getElem]; simp [Vector.get, h]
 
-@[simp] def permute_id (j : Fin n) {v : KellerVertex n s} : permute j id v = v := by
+@[simp] theorem permute_id (j : Fin n) {v : KVertex n s} : permute j id v = v := by
   simp [permute]
   congr
   ext i hi
   simp
 
-@[simp] def permute_comp (j : Fin n) (f₁ f₂ : Fin s → Fin s) {v} :
+@[simp] theorem permute_comp (j : Fin n) (f₁ f₂ : Fin s → Fin s) {v} :
     (permute j f₁ v).permute j f₂ = permute j (f₂ ∘ f₁) v := by
   simp [permute]
   ext i hi; simp
   split <;> simp
 
-end KellerVertex
+theorem permute_commute (j₁ j₂ : Fin n) (f₁ f₂ : Fin s → Fin s) {v} :
+    j₁ ≠ j₂ → (permute j₁ f₁ (permute j₂ f₂ v)) = permute j₂ f₂ (permute j₁ f₁ v) := by
+  intro hne
+  simp [permute]
+  ext i hi; simp
+  split <;> split <;> simp_all
 
-def KellerAdj (v₁ v₂ : KellerVertex n s) : Prop :=
+end KVertex
+
+def KAdj (v₁ v₂ : KVertex n s) : Prop :=
   ∃ (j₁ : Fin n),
       v₁.bv[j₁] ≠ v₂.bv[j₁] ∧ v₁.colors[j₁] = v₂.colors[j₁] ∧
     ∃ j₂, j₁ ≠ j₂ ∧
       (v₁.bv[j₂] ≠ v₂.bv[j₂] ∨ v₁.colors[j₂] ≠ v₂.colors[j₂])
 
-instance : DecidableRel (KellerAdj (n := n) (s := s)) :=
-  fun v₁ v₂ => by unfold KellerAdj; infer_instance
+instance : DecidableRel (KAdj (n := n) (s := s)) :=
+  fun v₁ v₂ => by unfold KAdj; infer_instance
 
-theorem KellerAdj.symm : Symmetric (KellerAdj (n := n) (s := s)) := by
+theorem KAdj.symm : Symmetric (KAdj (n := n) (s := s)) := by
   intro x y h
-  unfold KellerAdj at h ⊢
+  unfold KAdj at h ⊢
   rcases h with ⟨j₁,hb1,hc1,j₂,hj,h2⟩
   refine ⟨j₁,hb1.symm,hc1.symm,j₂,hj,?_⟩
   aesop
 
-theorem KellerAdj.irrefl : Irreflexive (KellerAdj (n := n) (s := s)) := by
-  intro x; unfold KellerAdj; simp
+theorem KAdj.irrefl : Irreflexive (KAdj (n := n) (s := s)) := by
+  intro x; unfold KAdj; simp
 
-def KellerGraph (n s) : SimpleGraph (KellerVertex n s) where
-  Adj := KellerAdj
-  symm := KellerAdj.symm
-  loopless := KellerAdj.irrefl
+def KGraph (n s) : SimpleGraph (KVertex n s) where
+  Adj := KAdj
+  symm := KAdj.symm
+  loopless := KAdj.irrefl
 
-@[simp] theorem KellerGraph.Adj : (KellerGraph n s).Adj = KellerAdj := rfl
+@[simp] theorem KGraph.Adj : (KGraph n s).Adj = KAdj := rfl
 
-theorem sameBV_not_adj (v₁ v₂ : KellerVertex n s)
-  : v₁.bv = v₂.bv → ¬ KellerAdj v₁ v₂ := by
+theorem sameBV_not_adj (v₁ v₂ : KVertex n s)
+  : v₁.bv = v₂.bv → ¬ KAdj v₁ v₂ := by
   intro h
-  unfold KellerAdj; simp [h]
+  unfold KAdj; simp [h]
+
+theorem KVertex.bv_injOn_clique (isClique : (KGraph n s).IsClique vs) :
+    vs.InjOn KVertex.bv := by
+  intro v₁ hv₁ v₂ hv₂ h
+  by_contra hne
+  have := sameBV_not_adj v₁ v₂ h
+  have := isClique hv₁ hv₂ hne
+  contradiction
+
+theorem clique_finite (isClique : (KGraph n s).IsClique vs) :
+    vs.Finite := by
+  apply Set.Finite.of_finite_image (f := KVertex.bv)
+  · exact Set.toFinite (KVertex.bv '' vs)
+  · apply KVertex.bv_injOn_clique
+    exact isClique
+
+theorem clique_to_nclique (isClique : (KGraph n s).IsClique vs) :
+  ∃ (size : Nat) (vs' : Finset _) (_eq : vs ≃ vs'),
+      (KGraph n s).IsNClique size vs' := by
+  have := clique_finite isClique
+  use this.toFinset.card, this.toFinset, this.subtypeEquivToFinset
+  rw [SimpleGraph.isNClique_iff]; simp [isClique]
+
+theorem nclique_card_le (isNClique : (KGraph n s).IsNClique size vs) :
+    vs.card ≤ 2^n := by
+  rw [← BitVec.card (n := n)]
+  apply Finset.card_le_card_of_injOn
+  · simp
+  · apply KVertex.bv_injOn_clique isNClique.isClique
 
 theorem sameBV_ind_set (i : BitVec n) :
-  (KellerGraph n s)ᶜ.IsClique (fun v => v.bv = i) := by
+  (KGraph n s)ᶜ.IsClique (fun v => v.bv = i) := by
   rw [SimpleGraph.isClique_iff]
   intro v1 h1 v2 h2 hne
   rw [SimpleGraph.compl_adj]
@@ -99,37 +142,67 @@ theorem sameBV_ind_set (i : BitVec n) :
   apply sameBV_not_adj
   rw [h1,h2]
 
-theorem maxClique_le : (KellerGraph n s).cliqueNum ≤ 2^n := by
+theorem maxClique_le : (KGraph n s).cliqueNum ≤ 2^n := by
   unfold SimpleGraph.cliqueNum
   generalize hsizes : setOf _ = sizes
   by_contra h
-  simp [] at h
+  simp at h
   have : ∃ size ∈ sizes, size > 2^n := by
     clear hsizes
     exact exists_lt_of_lt_csSup' h
   clear h
   simp [← hsizes] at this; clear hsizes
-  rcases this with ⟨size,⟨clique,⟨hclique,rfl⟩⟩,h⟩
-  have := Finset.exists_ne_map_eq_of_card_lt_of_maps_to
-    (s := clique) (t := Finset.univ (α := Fin (2^n)))
-    (f := fun v => v.bv.toFin)
-  simp [h, BitVec.toFin_inj] at this; clear h
-  rcases this with ⟨x, hx, y, hy, hne, h⟩
-  apply sameBV_not_adj x y h
-  apply hclique hx hy hne
+  rcases this with ⟨size,⟨clique,isNclique⟩,h⟩
+  have := nclique_card_le isNclique
+  have := isNclique.card_eq
+  omega
 
 
-def KellerAuto (n s) := SimpleGraph.Iso (KellerGraph n s) (KellerGraph n s)
+def KClique (vs : Finset (KVertex n s)) :=
+  (KGraph n s).IsNClique (2^n) vs
 
-def KellerAuto.flip (j : Fin (n+1)) : KellerAuto (n+1) s :=
+def KClique.get (i : BitVec n) (h : KClique (n := n) (s := s) vs)
+    : Vector (Fin s) n :=
+  vs.choose (fun v => v.bv = i) (by
+    apply existsUnique_of_exists_of_unique
+    · have := Finset.surj_on_of_inj_on_of_card_le
+        (s := vs) (t := Finset.univ) (f := fun a _ => a.bv)
+        (hf := by simp)
+        (hinj := fun _ _ c d => KVertex.bv_injOn_clique h.isClique c d)
+        (hst := by simp [h.card_eq])
+        i (by simp)
+      rcases this with ⟨v,hv,rfl⟩
+      use v, hv
+    · rintro x1 x2 ⟨h1,rfl⟩ ⟨h2,hbv⟩
+      apply KVertex.bv_injOn_clique h.isClique h1 h2 hbv.symm
+  ) |>.2
+
+theorem KClique.get_mem (i : BitVec n) (h : KClique (n := n) (s := s) vs)
+    : ⟨i, h.get i⟩ ∈ vs := by
+  unfold get
+  generalize hv : Finset.choose (fun v => v.bv = i) vs _ = v
+  have ⟨mem,prop⟩ : v ∈ vs ∧ v.bv = i := by
+    rw [← hv]; apply Finset.choose_spec
+  convert mem
+  exact prop.symm
+
+
+def conjectureIn (n : Nat) : Prop :=
+  ∀ s, ¬∃ vs : Finset (KVertex n s), KClique vs
+
+
+
+def KAuto (n s) := SimpleGraph.Iso (KGraph n s) (KGraph n s)
+
+def KAuto.flip (j : Fin (n+1)) : KAuto (n+1) s :=
   RelIso.mk ({
-    toFun := KellerVertex.flip j
-    invFun := KellerVertex.flip j
-    left_inv := by apply KellerVertex.flip_flip
-    right_inv := by apply KellerVertex.flip_flip
+    toFun := KVertex.flip j
+    invFun := KVertex.flip j
+    left_inv := by apply KVertex.flip_flip
+    right_inv := by apply KVertex.flip_flip
   }) (by
     intro v₁ v₂
-    simp [KellerAdj]
+    simp [KAdj]
     constructor
     · rintro ⟨j₁,hb1,hc1,j₂,hne,h2⟩
       replace hb1 : v₁.bv[j₁] ≠ v₂.bv[j₁] := by
@@ -152,16 +225,16 @@ def KellerAuto.flip (j : Fin (n+1)) : KellerAuto (n+1) s :=
       by_cases hj2 : j = j₂ <;> (simp [hj2]; exact h2)
     )
 
-def KellerAuto.permute (j : Fin (n+1)) (f : Fin s ≃ Fin s) : KellerAuto (n+1) s :=
+def KAuto.permute (j : Fin (n+1)) (f : Fin s ≃ Fin s) : KAuto (n+1) s :=
   RelIso.mk ({
-    toFun := KellerVertex.permute j f
-    invFun := KellerVertex.permute j f.symm
+    toFun := KVertex.permute j f
+    invFun := KVertex.permute j f.symm
     left_inv := by intro; simp
     right_inv := by intro; simp
   }
   ) (by
     intro v₁ v₂
-    simp [KellerAdj]
+    simp [KAdj]
     constructor
     · rintro ⟨j₁,hb1,hc1,j₂,hne,h2⟩
       replace hc1 : v₁.colors[j₁] = v₂.colors[j₁] := by
@@ -186,11 +259,16 @@ def KellerAuto.permute (j : Fin (n+1)) (f : Fin s ≃ Fin s) : KellerAuto (n+1) 
   )
 
 
+
+
+
+
+
 structure KellerCliqueData (n s : Nat) where
   vertices : Vector (Vector (Fin s) n) (2^n)
 deriving Repr
 
-def KellerCliqueData.get (i : BitVec n) (kc : KellerCliqueData n s): KellerVertex n s :=
+def KellerCliqueData.get (i : BitVec n) (kc : KellerCliqueData n s): KVertex n s :=
   { bv := i, colors := kc.vertices[i.toFin] }
 
 instance : ToString (KellerCliqueData n s) where
@@ -198,4 +276,4 @@ instance : ToString (KellerCliqueData n s) where
 
 
 def checkAll (kc : KellerCliqueData n s) : Bool :=
-  decide (∀ i i' : Fin (2^n), i < i' → KellerAdj (kc.get i) (kc.get i'))
+  decide (∀ i i' : Fin (2^n), i < i' → KAdj (kc.get i) (kc.get i'))
