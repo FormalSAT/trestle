9f13ceefea13283b52c16809e2c1c65274989790
James Gallicchio <jamesgallicchio@gmail.com>
2025-01-27 17:14:39 -0500

    chore(Keller): document and clean up SB proofs

--- Experiments/Keller/SymmBreak.lean
+++ Experiments/Keller/SymmBreak.lean
@@ -5,15 +5,24 @@ namespace Keller
 structure SB0 (n s) where
   kclique : KClique n s
 
+abbrev SB1.c0_colors : Vector (Fin (s+1)) (n+2) :=
+  ⟨Array.mkArray (n+2) 0, by simp⟩
+
+abbrev SB1.c1_colors : Vector (Fin (s+1)) (n+2) :=
+  ⟨#[0,1] ++ Array.mkArray n 0, by simp; omega⟩
+
 structure SB1 (n s) extends SB0 (n+2) (s+1) where
-  c0 : kclique.get 0 = ⟨Array.mkArray (n+2) 0, by simp⟩
-  c1 : kclique.get 1 = ⟨#[0,1] ++ Array.mkArray n 0, by simp; omega⟩
+  c0 : kclique.get 0 = SB1.c0_colors
+  c1 : kclique.get 1 = SB1.c1_colors
 
 theorem SB0.pick_pair {n s} (x : SB0 (n+2) (s+1)) (h : conjectureIn (n+1))
-  : ∃ (i₁ i₂ : BitVec (n+2)) (j₁ j₂ : Fin (n+2)), j₁ ≠ j₂ ∧ ∀ j (h : j < n+2),
-      (j ≠ j₁ ↔ i₁[j] = i₂[j]) ∧
-      (j ≠ j₂ ↔ (x.kclique.get i₁)[j] = (x.kclique.get i₂)[j])
+  : ∃ a ∈ x.kclique.val, ∃ b ∈ x.kclique.val,
+    ∃ (j₁ j₂ : Fin (n+2)), j₁ ≠ j₂ ∧
+      ∀ (j : ℕ) (h : j < n + 2),
+        (j ≠ ↑j₁ ↔ a.bv[j] = b.bv[j]) ∧
+        (j ≠ ↑j₂ ↔ a.colors[j] = b.colors[j])
   := by
+  -- construct a K_0 in smaller graph, using vertices with last bit 0 in bigger graph
   let K_0 : Finset (KVertex (n+1) (s+1)) := (Finset.univ (α := BitVec (n+1)))
     |>.map ⟨fun i =>
         let i' := i.cons false
@@ -23,6 +32,7 @@ theorem SB0.pick_pair {n s} (x : SB0 (n+2) (s+1)) (h : conjectureIn (n+1))
         intro x1 x2 heq
         simp at heq; exact heq.1⟩
   have K_0_card : K_0.card = (2^(n+1)) := by simp [K_0]
+  -- K_0 must not be a clique, because the conjecture holds in that dimension
   have K_0_not_clique : ¬ _ := (h (s+1)).false ∘ Subtype.mk K_0
   -- find the vertices in K_0 which are the not adjacent
   simp [KClique, SimpleGraph.isNClique_iff, K_0_card,
@@ -45,15 +55,16 @@ theorem SB0.pick_pair {n s} (x : SB0 (n+2) (s+1)) (h : conjectureIn (n+1))
     iterate 2 (apply KClique.get_mem)
     simp [i_diff]
   simp [KAdj] at this hnotadj
-  rcases this with ⟨⟨j₁,hj₁⟩,is_have_diff,ks_same,⟨j₂,hj₂⟩,js_diff,h⟩
+  -- by def of adjacency, we get a j₁, j₂ in big graph
+  rcases this with ⟨⟨j₁,hj₁⟩,is_diff_at_j1,ks_same,⟨j₂,hj₂⟩,js_diff,h⟩
   simp_all
   -- the i's are different at index j₁
-  have : j₁ ≠ n+1 := by intro contra; simp [BitVec.getElem_cons, contra] at is_have_diff
-  simp [BitVec.getElem_cons, this] at is_have_diff
+  have : j₁ ≠ n+1 := by intro contra; simp [BitVec.getElem_cons, contra] at is_diff_at_j1
   replace hj₁ : j₁ < n+1 := by omega
+  simp [BitVec.getElem_cons, this] at is_diff_at_j1
   clear this
-  specialize hnotadj ⟨j₁, hj₁⟩ is_have_diff ks_same
-  -- j₂ must be n+1
+  specialize hnotadj ⟨j₁, hj₁⟩ is_diff_at_j1 ks_same
+  -- j₂ must be n+1, because vertices in small graph are not adjacent
   by_cases this : j₂ = n+1
   case neg =>
     exfalso
@@ -64,9 +75,12 @@ theorem SB0.pick_pair {n s} (x : SB0 (n+2) (s+1)) (h : conjectureIn (n+1))
     apply h hnotadj.1
   subst this
   simp [BitVec.getElem_cons] at h
-  -- now we fill the goal
-  use (i₁.cons false), (i₂.cons false), ⟨j₁,‹_›⟩, ⟨n+1, by omega⟩
-  simp [hk₁, hk₂, js_diff]
+
+  -- Ok! We have all the info we need to fill the goal!
+  use ⟨i₁.cons false, k₁⟩, (hk₁ ▸ x.kclique.get_mem ..),
+      ⟨i₂.cons false, k₂⟩, (hk₂ ▸ x.kclique.get_mem ..),
+      ⟨j₁,‹_›⟩, ⟨n+1, by omega⟩
+  simp [js_diff]
   rintro j -
   by_cases eq_j1 : j = j₁
   · simp_all [BitVec.getElem_cons]
@@ -75,6 +89,8 @@ theorem SB0.pick_pair {n s} (x : SB0 (n+2) (s+1)) (h : conjectureIn (n+1))
     · specialize hnotadj ⟨j, by omega⟩ (by simp; exact Ne.symm eq_j1)
       simp_all [BitVec.getElem_cons]; exact hnotadj.1
 
+/-- The automorphism that reorders any columns `j₁`, `j₂`
+to the first and second column, respectively. -/
 def SB0.reorder (j₁ j₂ : Fin (n+2)) := KAuto.swap [(0,j₁), (1,j₂)]
 
 @[simp] theorem SB0.reorder_0 (j1 j2 : Fin (n+2)) : reorder j1 j2 0 = j1 := by
@@ -99,7 +115,7 @@ def SB0.auto {n s} (v₁ v₂ : KVertex (n+2) (s+2)) : KAuto (n+2) (s+2) :=
   )
 
 theorem SB0.auto_v₁ {v₁ v₂ : KVertex (n+2) (s+2)} :
-      (SB0.auto v₁ v₂).toFun v₁ = ⟨0, Vector.mkVector _ 0⟩ := by
+      (SB0.auto v₁ v₂).toFun v₁ = ⟨0, SB1.c0_colors⟩ := by
   ext j hj
   · unfold auto; simp [KVertex.bv_flip]
   · unfold auto; simp [KVertex.colors_permute, Vector.mkVector]
@@ -112,7 +128,7 @@ theorem SB0.auto_v₂ {v₁ v₂ : KVertex (n+2) (s+2)}
       (h : ∀ j (h : j < n+2),
           (j ≠ 0 ↔ v₁.bv[j] = v₂.bv[j]) ∧
           (j ≠ 1 ↔ v₁.colors[j] = v₂.colors[j])) :
-      (SB0.auto v₁ v₂).toFun v₂ = ⟨1, ⟨#[0,1] ++ Array.mkArray n 0, by simp; omega⟩⟩ := by
+      (SB0.auto v₁ v₂).toFun v₂ = ⟨1, SB1.c1_colors⟩ := by
   ext j hj <;> specialize h j hj
   · replace h := h.1
     unfold auto; simp [KVertex.bv_flip]
@@ -135,24 +151,29 @@ theorem SB0.auto_v₂ {v₁ v₂ : KVertex (n+2) (s+2)}
         simp [this]
       · rfl
 
-theorem SB0.to_SB1 {n s} (k : SB0 (n+2) (s+2)) (h : conjectureIn (n+1))
+theorem SB0.to_SB1 {n s} (sb0 : SB0 (n+2) (s+2)) (h : conjectureIn (n+1))
   : Nonempty (SB1 n (s+1)) := by
-  have ⟨ai, bi, j₁, j₂, hne, same_on⟩ := k.pick_pair h
-  -- clean up the context a *bunch*
-  -- TODO(JG): just change pick_pair to be in the form I actually want it in
-  clear h; rcases k with ⟨k⟩
-  generalize a_mem_vs : k.get ai = a_cs at same_on
-  generalize b_mem_vs : k.get bi = b_cs at same_on
-  rw [KClique.get_eq_iff_mem] at a_mem_vs b_mem_vs
-  generalize ha : KVertex.mk ai a_cs = a at a_mem_vs
-  generalize hb : KVertex.mk bi b_cs = b at b_mem_vs
-  simp [KVertex.ext_iff] at ha hb
-  rcases ha with ⟨rfl,rfl⟩; rcases hb with ⟨rfl,rfl⟩
-  -- okay, context is now pretty reasonable
+  have ⟨a, a_mem, b, b_mem, j₁, j₂, hne, same_on⟩ := sb0.pick_pair h
+  rcases sb0 with ⟨k⟩
   -- apply the reordering automorphism to get vs2, k2, a2, b2
   let k2 := k.map (KAuto.reorder <| SB0.reorder j₁ j₂)
   let a2 := (KAuto.reorder (reorder j₁ j₂)).toFun a
   let b2 := (KAuto.reorder (reorder j₁ j₂)).toFun b
+  have a2_mem : a2 ∈ k2.val := by apply Finset.mem_map_of_mem; exact a_mem
+  have b2_mem : b2 ∈ k2.val := by apply Finset.mem_map_of_mem; exact b_mem
+  replace same_on : ∀ (j : ℕ) (h : j < n + 2),
+      (j ≠ 0 ↔ a2.bv[j] = b2.bv[j]) ∧
+      (j ≠ 1 ↔ a2.colors[j] = b2.colors[j]) := by
+    intro j hj
+    simp [a2, b2, KVertex.bv_reorder, KVertex.colors_reorder]
+    constructor
+    · rw [← (same_on _ _).1, not_iff_not, Fin.val_eq_val,
+        SB0.reorder_eq_j1, ← Fin.val_eq_val]
+      rfl
+    · rw [← (same_on _ _).2, not_iff_not, Fin.val_eq_val,
+        SB0.reorder_eq_j2 _ _ hne, ← Fin.val_eq_val]
+      rfl
+
   -- apply the "move to all 0s" automorphism to get vs3, k3
   let k3 := k2.map (SB0.auto a2 b2)
 
@@ -160,22 +181,10 @@ theorem SB0.to_SB1 {n s} (k : SB0 (n+2) (s+2)) (h : conjectureIn (n+1))
   refine ⟨{kclique := k3, c0 := ?c0, c1 := ?c1}⟩
   case c0 =>
     rw [KClique.get_eq_iff_mem]; simp [k3, KClique.map]
-    use a2
-    constructor
-    · apply Finset.mem_map_of_mem; exact a_mem_vs
-    · apply SB0.auto_v₁
+    use a2, a2_mem
+    apply SB0.auto_v₁
   case c1 =>
     rw [KClique.get_eq_iff_mem]; simp [k3, KClique.map]
-    use b2
-    constructor
-    · apply Finset.mem_map_of_mem; exact b_mem_vs
-    · apply SB0.auto_v₂
-      intro j hj
-      simp [a2, b2, KVertex.bv_reorder, KVertex.colors_reorder]
-      constructor
-      · rw [← (same_on _ _).1, not_iff_not, Fin.val_eq_val,
-          SB0.reorder_eq_j1, ← Fin.val_eq_val]
-        rfl
-      · rw [← (same_on _ _).2, not_iff_not, Fin.val_eq_val,
-          SB0.reorder_eq_j2 _ _ hne, ← Fin.val_eq_val]
-        rfl
+    use b2, b2_mem
+    apply SB0.auto_v₂
+    exact same_on
