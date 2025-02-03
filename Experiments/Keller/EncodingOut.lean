feefc20ef4f56cc67533fefac011310475c5c871
James <jamesgallicchio@gmail.com>
2025-02-02 22:34:07 -0500

    feat(Keller): update CLI to have an --inc flag for inccnf

--- Experiments/Keller/EncodingOut.lean
+++ Experiments/Keller/EncodingOut.lean
@@ -11,26 +11,53 @@ import Trestle.Solver.IncCNF
 
 import Cli
 
-open Keller Trestle
+open Keller Trestle Cli
 
-def main (args : List String) := show IO _ from do
-  if args.length < 3 then
-    IO.println "command arguments: <n> <s> <cnf file>"
-    return
-  let n := args[0]!.toNat!
-  let s := args[1]!.toNat!
-  let file := args[2]!
+def cnfCmd := `[Cli|
+  cnfCmd VIA runCnfCmd;
+  "testCmd provides another example for a subcommand without flags or arguments that does nothing."
+
+  FLAGS:
+    inc : String;    "Output an incremental CNF (inccnf) with good cube split."
+
+  ARGS:
+    n : Nat;         "# dimensions of the Keller graph."
+    s : Nat;         "# colors available."
+    file : Nat;      "File path to output CNF."
+]
+where runCnfCmd (p : Parsed) := do
+  let n := p.positionalArg! "n" |>.as! Nat
+  let s := p.positionalArg! "s" |>.as! Nat
+  let file := p.positionalArg! "file" |>.as! String
+  let inc := p.hasFlag "inc"
+
   IO.println s!"encoding G_{n}_{s}"
   let ((), {cnf, vMap, ..}) := Encoding.fullEncoding n s |>.run
-  if h : n ≥ 5 ∧ s ≥ 4 then
-    let cubes := Encoding.symmBreakCubes (n := n) (s := s) h.1 h.2
+  if inc then
+    let cubes :=
+      if h : n ≥ 5 ∧ s ≥ 4 then
+        Encoding.symmBreakCubes (n := n) (s := s) h.1 h.2
+      else []
     let cubes := cubes.map (·.map _ vMap)
     IO.println s!"writing incremental CNF to {file}"
-    let () ← IO.FS.withFile file .write <| fun handle => do
+    IO.FS.withFile file .write <| fun handle => do
       Solver.Dimacs.printIncCNF (handle.putStr) cnf cubes
       handle.flush
   else
     IO.println s!"writing CNF to {file}"
-    let () ← IO.FS.withFile file .write <| fun handle => do
+    IO.FS.withFile file .write <| fun handle => do
       Solver.Dimacs.printFormula (handle.putStr) cnf
       handle.flush
+  return 0
+
+def mainCmd : Cmd := `[Cli|
+  mainCmd NOOP; ["0.0.1"]
+  "Keller conjecture SAT encoding formalization output."
+
+  SUBCOMMANDS:
+    cnfCmd
+
+]
+
+def main (args : List String) := show IO _ from do
+  mainCmd.validate args
