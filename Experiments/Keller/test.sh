b59c96593f79da74ea710e3a0fac418bc6d65cd0
James Gallicchio <jamesgallicchio@gmail.com>
2025-04-08 21:07:26 -0400

    feat(Keller): test script check tautology, shuffle cubes before solving

--- Experiments/Keller/test.sh
+++ Experiments/Keller/test.sh
@@ -10,6 +10,8 @@ CUBES="$DIR/g${N}_${S}_cubes.icnf"
 LSR="$DIR/g${N}_${S}.lsr"
 SB="$DIR/g${N}_${S}_sb.cnf"
 
+TAUTO="$DIR/g${N}_${S}_tauto.cnf"
+
 TMPDIR="tmp"
 
 set -e -x
@@ -19,24 +21,15 @@ PATH="/home/james/Projects/sat/dsr-trim/src:/home/james/Projects/sat/drat-trim:$
 
 (cd ../..; LEAN_ABORT_ON_PANIC=1 lake exe keller cnf $N $S --cnf $CNF --dsr $DSR --cube $CUBES)
 
+# check the SR proof
 time dsr-trim -f $CNF $DSR $LSR --emit-valid-formula-to=$SB
 lsr-check $CNF $LSR
 
-if [ -d $TMPDIR ]
-then
-    rm -r $TMPDIR
-fi
+# check that the cubes negated leads to tautology
+(cd ../..; LEAN_ABORT_ON_PANIC=1 lake exe keller negate-cubes --cnf $SB --cubes $CUBES --out $TAUTO)
+cadical $TAUTO
 
-mkdir $TMPDIR
+# shuffle the cubes and start runnin em
+(echo "p inccnf"; grep -v "^p" $CNF; cat $CUBES | shuf) |
+    cadical
 
-line_number=0
-while IFS= read -r cube; do
-    TMP="tmp/cube$line_number.cnf"
-    cat <(echo "p inccnf") <(grep -v "^p" $SB) <(echo "$cube") > $TMP
-    cadical $TMP
-    rm $TMP
-
-    line_number=$(( $line_number + 1 ))
-done < "$CUBES"
-
-rmdir $TMPDIR
