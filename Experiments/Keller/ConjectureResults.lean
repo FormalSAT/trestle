b1b4289f602443c199a92affe86db7a714c2bab1
James <jamesgallicchio@gmail.com>
2025-04-10 14:30:02 -0400

    feat(Keller): broken -- attempt to fix pick_pair to more lenient bound on s

--- Experiments/Keller/ConjectureResults.lean
+++ Experiments/Keller/ConjectureResults.lean
@@ -5,9 +5,26 @@ Released under the Apache License v2.0; see LICENSE for full text.
 Authors: James Gallicchio
 -/
 
+import Experiments.Keller.Encoding
 import Experiments.Keller.G8_Clique
 
+import Experiments.Solver.Builtin
+
 namespace Keller
+
+theorem conjectureIn_of_cnf_unsat {n : Nat} (h : conjectureIn (n+2)) :
+  (Encoding.fullEncoding (n+3) (2^(n+2))).val.toICnf.Unsat → conjectureIn (n+3) := by
+  intro unsat
+  unfold conjectureIn
+  by_contra ex
+  simp at ex
+  rcases ex with ⟨c⟩
+  have : 2 ≤ 2^(n+2) := Nat.le_self_pow (by simp) _
+  have ⟨tc⟩ := SymmBreak.C3Zeros.ofClique (s := 2^(n+1)-2) h (by omega) (cast (by congr; omega) c)
+  have := Encoding.cliqueToAssn_satisfies_fullSpec
+  unfold Trestle.ICnf.Unsat Trestle.ICnf.Sat Trestle.Model.PropFun.satisfiable
+  rw [Trestle.Encode.EncCNF.encodesProp_equisatisfiable]
+  sorry
 
 /-! ## Positive Results -/
 
@@ -15,9 +32,12 @@ theorem conjectureIn_2 : conjectureIn 2 := by
   unfold conjectureIn
   rw [← KCliqueData.checkAll_iff_isempty_kclique]
   native_decide
+
 #print axioms conjectureIn_2
 
-theorem conjectureIn_3 : conjectureIn 3 := sorry
+theorem conjectureIn_3 : conjectureIn 3 := by
+
+  sorry
 theorem conjectureIn_4 : conjectureIn 4 := sorry
 theorem conjectureIn_5 : conjectureIn 5 := sorry
 theorem conjectureIn_6 : conjectureIn 6 := sorry
