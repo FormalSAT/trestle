44d0db7f9c0d4719e533c942c69a6cabf8780044
James <jamesgallicchio@gmail.com>
2025-04-27 04:01:21 -0400

    feat(Trestle): conditional naive cardinality constraints

--- Trestle/Encode/Cardinality/GeneralEncs.lean
+++ Trestle/Encode/Cardinality/GeneralEncs.lean
@@ -13,34 +13,48 @@ namespace Trestle.Encode.Cardinality
 
 open VEncCNF Model PropFun
 
-/-- Naive less-than-k encoding, using `(n choose k)` clauses. -/
-def naiveLtK (k : Nat) (lits : Array (Literal ν)) :
-  VEncCNF ν Unit (lessThan k (Multiset.ofList lits.toList)) :=
+/-- Conditional, naive less-than-k encoding, using `(n choose k)` clauses.
+This is identical to `naiveLtK`, but accepts an array of literals to be prepended to every clause.
+-/
+def naiveLtK.cond (cond : Clause (Literal ν)) (k : Nat) (lits : Array (Literal ν)) :
+  VEncCNF ν Unit (condᶜ ⇨ lessThan k (Multiset.ofList lits.toList)) :=
   -- among all subsets `S ⊆ lits` with `|S| = k`, at least one of the literals is false
   (for_all (lits.toList.sublistsLen k).toArray fun sublist =>
-    addClause <| (sublist.toArray.map (-·))
+    addClause <| (cond ++ sublist.toArray.map (-·))
   ) |>.mapProp (by
     ext τ; rcases lits with ⟨lits⟩
     rw [← not_iff_not]
-    conv => lhs; simp [Clause.satisfies_iff]
-    conv => rhs; simp [card, List.countP_eq_length_filter]
+    conv => lhs; simp [Clause.satisfies_iff, LitVar.neg_eq_iff_eq_neg]
+    conv => rhs; simp [Clause.satisfies_iff, card, List.countP_eq_length_filter]
     constructor
     · rintro ⟨S,S_sub,rfl,all_true⟩
-      apply List.Sublist.length_le
-      calc
-        _ = _ := by rw [eq_comm, List.filter_eq_self]; simpa using all_true
-        List.Sublist _ _ := S_sub.filter _
-    · intro length_ge
-      simp at length_ge
+      constructor
+      · intro x x_mem
+        apply all_true; left; exact x_mem
+      · apply List.Sublist.length_le
+        convert S_sub.filter _
+        rw [eq_comm, List.filter_eq_self]
+        intro x x_mem
+        specialize all_true (-x) (by simp [x_mem])
+        simpa using all_true
+    · rintro ⟨cond_satisfied,length_ge⟩
       use lits.filter (τ ⊨ LitVar.toPropFun ·) |>.take k
       refine ⟨?_,?_,?_⟩
       · apply List.Sublist.trans
         · apply List.take_sublist
         · apply List.filter_sublist
       · simpa using length_ge
-      · intro a ha; replace ha := List.mem_of_mem_take ha
-        simp_all
+      · rintro a (a_mem|a_mem)
+        · exact cond_satisfied _ a_mem
+        · replace a_mem := List.mem_of_mem_take a_mem
+          simp_all
   )
+
+/-- Naive less-than-k encoding, using `(n choose k)` clauses. -/
+def naiveLtK (k : Nat) (lits : Array (Literal ν)) :
+  VEncCNF ν Unit (lessThan k (Multiset.ofList lits.toList)) :=
+  naiveLtK.cond #[] k lits
+  |>.mapProp (by ext τ; simp)
 
 /-- Naive at-least-k encoding -/
 def naiveAtLeastK (k : Nat) (lits : Array (Literal ν)) :
