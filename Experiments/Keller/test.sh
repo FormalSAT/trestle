26a0c2fb28f322aca788e321e2fb5c35f7c1dc09
James Gallicchio <jamesgallicchio@gmail.com>
2025-07-05 10:20:07 -0400

    chore(Keller): update test script

--- Experiments/Keller/test.sh
+++ Experiments/Keller/test.sh
@@ -5,12 +5,13 @@ DIR="$PWD/cnfs"
 
 CNF="$DIR/g${N}_${S}.cnf"
 DSR="$DIR/g${N}_${S}.dsr"
-CUBES="$DIR/g${N}_${S}_cubes.icnf"
+CUBES="$DIR/g${N}_${S}_cubes.dnf"
 
 LSR="$DIR/g${N}_${S}.lsr"
 SB="$DIR/g${N}_${S}_sb.cnf"
-DRAT="$DIR/g${N}_${S}_sb_proof.drat"
+SB_DRAT="$DIR/g${N}_${S}_sb_proof.drat"
 
+INC="$DIR/g${N}_${S}_sb_cube.icnf"
 TAUTO="$DIR/g${N}_${S}_tauto.cnf"
 
 set -e -x
@@ -19,25 +20,31 @@ set -e -x
 
 PATH="$PWD/../../.lake/build/bin:$PATH"
 
+# generate the CNF, the DSR proof, and the cubes
 keller cnf $N $S --cnf $CNF --dsr $DSR --cube $CUBES
+
+# can also use the C encoder to generate CNF
+gcc Keller-encode.c && ./a.out $N $S > $CNF
 
 # check the SR proof
 time dsr-trim -f $CNF $DSR $LSR
 lsr-check $CNF $LSR
 #srcheck $CNF $LSR
 
-# make the CNF with SB clauses
+# append the SR proven clauses
 keller append-sr-clauses --cnf $CNF --sr $DSR --out $SB
 
-USE_CUBES=false
+# append the cubes
+(echo "p inccnf"; grep -v "^p" $SB; cat $CUBES) > $INC
+
+USE_CUBES=true
 if $USE_CUBES; then
   # check that the cubes negated leads to tautology
   keller negate-cubes --cnf $SB --cubes $CUBES --out $TAUTO
   cadical $TAUTO || true
 
-  # shuffle the cubes and start runnin em
-  (echo "p inccnf"; grep -v "^p" $SB; cat $CUBES | shuf) |
-      cadical
+  cadical --forcephase=1 $INC
 else
-  cadical $SB $DRAT
+  # run it without the cubes
+  cadical --forcephase=1 $SB $SB_DRAT
 fi
