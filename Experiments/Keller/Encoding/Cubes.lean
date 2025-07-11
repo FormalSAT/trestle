6842ccd57203396dd7e3080a1ad3a650d73319f4
James Gallicchio <jamesgallicchio@gmail.com>
2025-07-11 12:27:19 -0400

    feat(Keller): better cubing (tauto does NOT yet pass)

--- Experiments/Keller/Encoding/Cubes.lean
+++ Experiments/Keller/Encoding/Cubes.lean
@@ -17,12 +17,32 @@ open Trestle Encode
 
 namespace Cubes
 
-open Vars
+abbrev CubeM (n s) := Prod <| Array (Cube (Literal (Vars n s)))
+def CubeM.bind (x : CubeM n s α) (f : α → CubeM n s β) : CubeM n s β :=
+  let (cubes1,a) := x
+  let (cubes2,b) := f a
+  (cubes1++cubes2,b)
+def CubeM.pure (a : α) : CubeM n s α := (#[],a)
+instance : Monad (CubeM n s) where
+  bind := CubeM.bind
+  pure := CubeM.pure
 
-def matrixCubes (n s) : Cubing <| Literal (Vars n s) :=
+def CubeM.split (c : Cube (Literal (Vars n s))) (m : CubeM n s α) : CubeM n s α :=
+  let (cubes,a) := m
+  let cubes := if cubes.isEmpty then #[#[]] else cubes
+  ( cubes.map (Array.append c ·)
+  , a)
+
+def CubeM.emit (c : Cube (Literal (Vars n s))) : CubeM n s Unit := (#[c],())
+def CubeM.emitMany (cs : Array (Cube (Literal (Vars n s)))) : CubeM n s Unit := (cs,())
+
+def CubeM.toCubing (c : CubeM n s Unit) : Cubing (Literal (Vars n s)) := c.1.toList
+
+open Vars CubeM
+
+def oldMatrixCubes (n s) : Cubing <| Literal (Vars n s) :=
   if h : n ≥ 5 ∧ s ≥ 4 then
     let matrixList := (SR.canonicalMats.get 3).canonical.toList
-    let idxs := #[7, 11, 19]
     let two : Fin n := ⟨2,by omega⟩
     let three : Fin n := ⟨3,by omega⟩
     let four : Fin n := ⟨4,by omega⟩
@@ -38,30 +58,75 @@ def matrixCubes (n s) : Cubing <| Literal (Vars n s) :=
   else
     .unit
 
+def matrixCubes (n s) :=
+  if h : n ≥ 5 ∧ s ≥ 2 then
+    let (easy, med, hard) := ([
+      #v[false,  true,  true,  true, false, false], -- trivial
+    ], [
+      #v[false, false, false, false, false, false],
+      #v[false,  true,  true, false, false, false],
+      #v[false,  true, false,  true, false, false],
+      #v[false,  true, false, false, false, false],
+      #v[false, false, false, false,  true,  true],
+    ], [
+      #v[false,  true,  true, false, false,  true], -- hardest
+    ])
+    let two : Fin n := ⟨2,by omega⟩
+    let three : Fin n := ⟨3,by omega⟩
+    let four : Fin n := ⟨4,by omega⟩
+    have : NeZero s := ⟨by omega⟩
+    let map : Vector Bool 6 → Array (Literal (Vars n s)) := fun m =>
+      #[.mk (x 7  three 0) m[0]
+      , .mk (x 7  four  0) m[1]
+      , .mk (x 11 two   0) m[2]
+      , .mk (x 11 four  0) m[3]
+      , .mk (x 19 two   0) m[4]
+      , .mk (x 19 three 0) m[5]
+      ]
+    (easy.map map, med.map map, hard.map map)
+  else
+    (Cubing.unit, Cubing.unit, Cubing.unit)
+
 def lastColsCubes (n s) : Cubing <| Literal (Vars n s) :=
-  if h : s > 1 then
-    if h : n = 7 then
-      let colorings := SR.col5n_colorings s (by omega) |>.filterMap (·.getLeft?)
-      colorings.map fun coloring =>
-        Array.flatten <| Array.ofFn (n := 2) fun col => Array.ofFn (n := 4) fun row =>
-          .pos (x #v[3,7,11,19][row] (h ▸ #[5,6][col]) coloring[col][row])
-    else if h : n = 6 then
-      let colorings := SR.col5_colorings s (by omega) |>.filterMap (·.getLeft?)
-      let js := List.finRange n |>.filter (·.val ≥ 5)
-      let cubes : List (Cubing _) := js.map fun j =>
-        colorings.map fun coloring =>
-          Array.ofFn (n := 4) fun idx =>
-            .pos (x #v[3,7,11,19][idx] j coloring[idx])
-      cubes.foldl (·.prod ·) (.unit)
-      else .unit
+  if h : n ≥ 5 ∧ s > 1 then
+    have : NeZero s := ⟨by omega⟩
+    (show CubeM n s Unit from
+      let rec iter (j : Nat) (start : Nat) := do
+        if hj : j < n then
+          for hi : i in [start:16] do
+            let cube : Cube (Literal (Vars n s)) := #[
+              .mk (x  3 ⟨j,hj⟩ 0) (i &&& 8 = 0)
+            , .mk (x  7 ⟨j,hj⟩ 0) (i &&& 4 = 0)
+            , .mk (x 11 ⟨j,hj⟩ 0) (i &&& 2 = 0)
+            , .mk (x 19 ⟨j,hj⟩ 0) (i &&& 1 = 0)
+            ]
+            split cube (iter (j+1) i)
+      iter 5 0
+    ).toCubing
+  else .unit
+
+def extraSplits (n s) : Cubing <| Literal (Vars n s) :=
+  if h : n = 7 ∧ s > 0 then
+    have : NeZero s := ⟨by omega⟩
+    let vars : List (Vars n s) := [
+      (x 2 ⟨3,by omega⟩ 0),
+      (x 2 ⟨4,by omega⟩ 0),
+      (x 4 ⟨3,by omega⟩ 0),
+      (x 4 ⟨4,by omega⟩ 0),
+      (x 6 ⟨3,by omega⟩ 0),
+      (x 6 ⟨4,by omega⟩ 0),
+    ]
+    vars.foldr (fun v => .prod [#[.pos v], #[.neg v]]) .unit
   else .unit
 
 def allCubes (n s) : List (Clause <| Literal <| Vars n s) :=
-  let matCubes := matrixCubes n s
+  let (easyMats,medMats,hardMats) := matrixCubes n s
   let lastColsCubes := lastColsCubes n s
 
-  let allCubes := matCubes.prod lastColsCubes
+  let allCubes := easyMats
+    ++ (medMats.prod lastColsCubes).prod (extraSplits n s)
+    ++ (hardMats.prod lastColsCubes).prod (extraSplits n s)
 
-  allCubes.filter (·.size > 0)
+  allCubes
 
 end Cubes
