49b210b7e80ce3a63ecc03ae58526b2d134e23fb
James Gallicchio <jamesgallicchio@gmail.com>
2025-07-17 00:44:08 -0400

    feat(Keller): update test script to produce proofs for cubes

--- Experiments/Keller/test.sh
+++ Experiments/Keller/test.sh
@@ -1,18 +1,25 @@
 N=6
 S=32
 
-DIR="$PWD/cnfs"
+DIR="$PWD/cnfs/g${N}_${S}"
 
-CNF="$DIR/g${N}_${S}.cnf"
-DSR="$DIR/g${N}_${S}.dsr"
-CUBES="$DIR/g${N}_${S}_cubes.dnf"
+mkdir -p $DIR
 
-LSR="$DIR/g${N}_${S}.lsr"
-SB="$DIR/g${N}_${S}_sb.cnf"
-SB_DRAT="$DIR/g${N}_${S}_sb_proof.drat"
+CNF="$DIR/keller.cnf"
+SB_DSR="$DIR/symbreak.dsr"
+CUBES="$DIR/cubes.dnf"
 
-INC="$DIR/g${N}_${S}_sb_cube.icnf"
-TAUTO="$DIR/g${N}_${S}_tauto.cnf"
+SB_LSR="$DIR/symbreak.lsr"
+CNF_SB="$DIR/keller_sb.cnf"
+
+ICNF="$DIR/keller_sb_cubes.icnf"
+TAUTO="$DIR/keller_sb_cubes_tauto.cnf"
+
+DRAT_SB="$DIR/keller_sb_proof.drat"
+DRAT_SB_OPT="$DIR/keller_sb_proof_opt.drat"
+
+DSR_FULL="$DIR/proof.dsr"
+LSR_FULL="$DIR/proof.lsr"
 
 set -e -x
 
@@ -21,32 +28,37 @@ set -e -x
 PATH="$PWD/../../.lake/build/bin:$PATH"
 
 # generate the CNF, the DSR proof, and the cubes
-keller cnf $N $S --cnf $CNF --dsr $DSR --cube $CUBES
+keller cnf $N $S --cnf $CNF --dsr $SB_DSR --cube $CUBES
 
 # can also use the C encoder to generate CNF
 #gcc Keller-encode.c && ./a.out $N $S > $CNF
 
 # check the SR proof
-time dsr-trim -f $CNF $DSR $LSR
-lsr-check $CNF $LSR
+time dsr-trim -f $CNF $SB_DSR $SB_LSR
+#lsr-check $CNF $LSR
 #srcheck $CNF $LSR
 
 # append the SR proven clauses
-keller append-sr-clauses --cnf $CNF --sr $DSR --out $SB
+keller append-sr-clauses --cnf $CNF --sr $SB_DSR --out $CNF_SB
 
-# combine CNF with cubes
-(echo "p inccnf"; grep -v "^p" $SB; cat $CUBES) > $INC
-# generate corresponding tautology check
-keller negate-cubes --cnf $SB --cubes $CUBES --out $TAUTO
 
 USE_CUBES=true
 if $USE_CUBES; then
-  # check that the cubes negated leads to tautology
-  cadical $TAUTO || true
+  # combine CNF with cubes
+  (echo "p inccnf"; grep -v "^p" $CNF_SB; cat $CUBES; echo "a 0") > $ICNF
 
-  mkdir "$DIR/g${N}_${S}_sb_cube"
-  ./run_par.sh $INC "$DIR/g${N}_${S}_sb_cube"
+  icadical --no-binary --skeletonIncremental $ICNF $DRAT_SB \
+    || true
+  #mkdir "$DIR/g${N}_${S}_sb_cube"
+  #./run_par.sh $INC "$DIR/g${N}_${S}_sb_cube"
 else
   # run it without the cubes
-  cadical --forcephase=1 --scorefactor=500 $SB $SB_DRAT
+  cadical --forcephase=1 --scorefactor=500 $CNF_SB $DRAT_SB
 fi
+
+drat-trim $CNF_SB $DRAT_SB -l $DRAT_SB_OPT
+
+(cat $SB_DSR $DRAT_SB_OPT | grep -v "^c" | grep -v "^d") > $DSR_FULL
+
+dsr-trim $CNF $DSR_FULL $LSR_FULL
+lsr-check $CNF $LSR_FULL
